847f4fc26a4f9082bae92326966f2c216d0d61bb
ocegu <ocegu@Wings.dhcp.wustl.edu>
2023-01-19 13:40:36 -0600

    Finish up message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Say hi to your group mates!");
+		System.out.println("Say hi to your group mates!"
+				+ "Hello, how are you all? Super excited to learn more from this class!");
 	}
 }
